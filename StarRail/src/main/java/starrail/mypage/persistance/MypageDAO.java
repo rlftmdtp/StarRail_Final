24416258dc6fb443ca5b92592a907eed4071faf4
Yung <Yung@Yung-PC>
2017-12-12 16:12:13 +0900

    12일 커밋

--- StarRail/src/main/java/starrail/mypage/persistance/MypageDAO.java
+++ StarRail/src/main/java/starrail/mypage/persistance/MypageDAO.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import starrail.course.domain.CourseDetailVO;
 import starrail.course.domain.CourseVO;
-//import starrail.reservation.domain.ReservationVO;
+import starrail.reservation.domain.ReservationVO;
 
 public interface MypageDAO {
 	
 	public List<CourseVO> courseList(String m_id) throws Exception;
-	//public List<ReservationVO> reservationList(String m_id) throws Exception;
+	public List<ReservationVO> reservationList(String m_id) throws Exception;
 	public void courseDelete(Integer c_id) throws Exception;
 	public void courseDetailDelete(Integer c_id) throws Exception;
 	
